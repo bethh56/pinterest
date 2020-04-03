097d020075564c4dda1a4aa83161b1fb992b5438
Beth <bethhnielsen56@gmail.com>
2020-04-03 11:32:38 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,11 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+import auth from './components/auth/auth';
 import '../styles/main.scss';
 
-console.error('hey');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  auth.loginButton();
+};
+
+init();
