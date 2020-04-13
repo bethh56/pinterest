63522d2e4f83c21631fe44dc188278cf8b19ad24
Beth <bethhnielsen56@gmail.com>
2020-04-12 21:06:35 -0500

    wip

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,9 +1,8 @@
 import boardData from '../../helpers/data/boardData';
-import pinData from '../../helpers/data/pinData';
 import utils from '../../helpers/utils';
 
-const singlePinView = () => {
-  pinData.getPins();
+const viewBoardsPins = () => {
+  console.error('button working');
 };
 
 const buildBoards = () => {
@@ -16,16 +15,16 @@ const buildBoards = () => {
         domString += `<h5 class="card-title border-bottom text-center pb-2">${board.name}</h5>`;
         domString += `<p class="card-text">${board.description}</p>`;
         domString += '<div class="d-flex justify-content-around">';
-        domString += `<button id="viewPins" class=" col-5 btn btn-success">View ${board.name} <i class="far fa-eye"></i></button>`;
+        domString += `<button class="viewPins col-5 btn btn-success">View ${board.name} <i class="far fa-eye"></i></button>`;
         domString += `<button id="deleteBoard" class="col-5 btn btn-danger ml-auto">Delete ${board.name} <i class="fas fa-trash"></i></button>`;
         domString += '</div>';
         domString += '</div>';
         domString += '</div>';
       });
       utils.printToDom('boardDisplay', domString);
+      $('.viewPins').click(viewBoardsPins);
     })
     .catch((err) => console.error('get boards broke', err));
-  $('#viewPins').click(singlePinView);
 };
 
 export default { buildBoards };
