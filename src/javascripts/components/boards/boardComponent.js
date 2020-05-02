d95338a7e6d41611881b02021dbe1f7ead4d0409
Beth <bethhnielsen56@gmail.com>
2020-05-02 00:05:43 -0500

    user able to add boards

--- src/javascripts/components/boards/boardComponent.js
+++ src/javascripts/components/boards/boardComponent.js
@@ -5,6 +5,23 @@ import addBoardModal from '../addBoardModal/addBoardModal';
 import boardData from '../../helpers/data/boardData';
 import utils from '../../helpers/utils';
 
+const saveBoard = (e) => {
+  e.preventDefault();
+  const newBoard = {
+    name: $('#boardName').val(),
+    description: $('#BoardDescription').val(),
+    UserUid: utils.getMyUid(),
+  };
+  boardData.addBoard(newBoard)
+    .then(() => {
+      document.getElementById('boardForm').reset();
+      $('#boardModal').modal('hide');
+      // eslint-disable-next-line no-use-before-define
+      buildBoards();
+    })
+    .catch((err) => console.error('Save New Board failed', err));
+};
+
 const removeBoard = (e) => {
   const boardId = e.target.closest('.card').id;
   boardData.deleteBoard(boardId)
@@ -31,7 +48,8 @@ const buildBoards = () => {
 const boardEvents = () => {
   $('body').on('click', '.viewPins', pins.buildPins);
   $('body').on('click', '.deleteBoard', removeBoard);
-  $('body').on('click', '#addBoardModalButton', addBoardModal.newFoodModal);
+  $('body').on('click', '#addBoardModalButton', addBoardModal.newBoardModal);
+  $('body').on('click', '#newBoardSubmit', saveBoard);
 };
 
 export default { buildBoards, boardEvents };
